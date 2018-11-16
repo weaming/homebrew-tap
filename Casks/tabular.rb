7bfa9064d09572a4579f72c9b11aad80437e0b47
weaming <garden.yuen@gmail.com>
2018-11-16 16:08:28 +0800

    add tabular

---
+++ Casks/tabular.rb
@@ -0,0 +1,10 @@
+cask 'tabular' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://192.168.3.166:8000/s/tabular'
+  name 'tabular'
+  homepage 'https://github.com/weaming/homebrew-tap'
+
+  # app ''
+end
